dea48229f602e90cec5cb7e86980d4a1db44880c
Arif Khan <ifeeforall@gmail.com>
2015-09-02 15:35:28 +0200

    adaptaton file changes

--- ellavator/run_adaptation.sh
+++ ellavator/run_adaptation.sh
@@ -3,18 +3,34 @@
 bw='/usr/local/libexec/sphinxtrain/bw'
 mllr_solve='/usr/local/libexec/sphinxtrain/mllr_solve'
 map_adapt='/usr/local/libexec/sphinxtrain/map_adapt'
+cur_dir=`pwd`
 
+if [ "$#" -ne 1 ]; then
+  echo "==================================="
+  echo "Usage: $0 mllr OR $0 map" >&2
+  echo "==================================="
+  exit 1
+fi
+
+adapt=$1
 fileids='etc/ellavator_train.fileids'
 dic='etc/ellavator.dic'
 trans='etc/ellavator_train.transcription'
 
-echo "Copying old model"
-rm -rf en-us && cp -r wsj-mdl/en-us . || exit 1;
+if [ ! -f cmusphinx-en-us-5.2.tar.gz ]; then
+rm -rf $cur_dir/en-us && echo "Deleting old model"
+echo "Getting the model"
+wget http://sourceforge.net/projects/cmusphinx/files/\
+Acoustic%20and%20Language%20Models/US%20English%20Generic%20Acoustic%20Model/cmusphinx-en-us-5.2.tar.gz
+echo "extracting archive"
+tar -xvf cmusphinx-en-us-5.2.tar.gz
+mv cmusphinx-en-us-5.2 en-us
+fi
+
 echo "==================================="
 echo "Extract mfcc features from training data"
 echo "==================================="
-sphinx_fe -argfile etc/feat.params -samprate 16000 -c $fileids \
-		-di . -do . -ei wav -eo mfc -mswav yes
+sphinx_fe -argfile en-us/feat.params -samprate 16000 -c $fileids -di $cur_dir/etc -do $cur_dir/etc -ei wav -eo mfc -mswav yes
 		
 echo "==================================="		
 echo "Convert model to text format"
@@ -24,20 +40,26 @@ pocketsphinx_mdef_convert -text en-us/mdef en-us/mdef.txt
 echo "==================================="
 echo "Run baum welche algorithm"
 echo "==================================="
-$bw -hmmdir en-us -moddeffn en-us/mdef.txt -ts2cbfn .ptm. -feat 1s_c_d_dd  \
-		-svspec 0-12/13-25/26-38 -cmn current -agc none -dictfn $dic \
-		-ctlfn $fileids -lsnfn $trans -accumdir .
+ 
+$bw -hmmdir en-us -moddeffn en-us/mdef.txt -ts2cbfn .cont. -feat 1s_c_d_dd \
+		-cmn current -agc none -lda en-us/feature_transform -cepdir etc -dictfn $dic \
+		-ctlfn $fileids -lsnfn $trans -accumdir $cur_dir
 
+if [ $adapt = mllr ]; then
 echo "==================================="
 echo "MLLR adaptation"
 echo "==================================="
-$mllr_solve -meanfn en-us/means -varfn en-us/variances -outmllrfn mllr_matrix -accumdir .
+$mllr_solve -meanfn en-us/means -varfn en-us/variances -outmllrfn mllr_matrix -accumdir $cur_dir
+
+fi
+
+if [ $adapt = map ]; then
 
 echo "==================================="
 echo "MAP adaptation"
 echo "==================================="
-$map_adapt -moddeffn en-us/mdef.txt -ts2cbfn .ptm. -meanfn en-us/means -varfn en-us/variances \
-		-mixwfn en-us/mixture_weights -tmatfn en-us/transition_matrices -accumdir . -mapmeanfn en-us-adapt/means \
+mkdir -p $cur_dir/en-us-adapt
+$map_adapt -moddeffn en-us/mdef.txt -ts2cbfn .cont. -meanfn en-us/means -varfn en-us/variances \
+		-mixwfn en-us/mixture_weights -tmatfn en-us/transition_matrices -accumdir $cur_dir -mapmeanfn en-us-adapt/means \
         -mapvarfn en-us-adapt/variances -mapmixwfn en-us-adapt/mixture_weights -maptmatfn en-us-adapt/transition_matrices
-        
-        
+fi
