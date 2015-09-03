01b953fae6de54f35cd7c053b9851419e3d75361
Arif Khan <arif.khan@dkfi.de>
2015-09-03 15:55:34 +0200

    Change data_prep and am-adaption scripts to be used for adaptation

--- ellavator/data_prep.sh
+++ ellavator/data_prep.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
 # This (ele_proj) is the directory of the Ellavator project on the github.
+# Change the ele_proj path below to your own project.
+# The (etcdir) is the direcotry inside the am_training/ellavator repository
+# Change this to your own path.
+
 ele_proj='/Users/arif/workspace/Ellavator_Dir/Ellavator'
+etcdir='/Users/arif/workspace/am_training/ellavator/etc'
+
+if [ "$#" -ne 1 ]; then
+  echo "==================================="
+  echo "Usage: $0 close OR $0 noise OR $0 both" >&2
+  echo "==================================="
+  exit 1
+fi
+
+adapt_data=$1
+etcdir='/Users/arif/workspace/am_training/ellavator/etc'
+
+if [ $adapt_data = both ]; then
+adapt_data='noise|close'
+fi
 
 ln -s $ele_proj/prompts/build/resources/data/ wav
 
 cd wav 
 
-etcdir='/Users/arif/workspace/am_training/ellavator/etc'
-txtids=`ls *.txt | grep 'en' | cut -d . -f 1`
-fileids=`ls *.wav | grep 'close' | grep 'en' | cut -d . -f 1`
+txtids=`ls *.txt | grep 'en' | grep -E $adapt_data | cut -d . -f 1`
+
+echo "==================================="
+echo "adaptation on $adapt_data data"
+echo "==================================="
+fileids=`ls *.wav | grep 'en' | grep -E $adapt_data | cut -d . -f 1`
 
 echo "making fileids file"
 rm -rf $etcdir/ellavator_train.fileids
@@ -17,14 +39,9 @@ for f in $fileids;do `echo $f >> $etcdir/ellavator_train.fileids`;done;
 
 echo "making training transcription file"
 rm -rf $etcdir/ellavator_train.transcription
-for f in $txtids;do echo "<s> `cat $f.txt | tr [:upper:] [:lower:]` </s> (${f}_close_16)" \
+for f in $txtids;do echo "<s> `cat $f.txt | tr [:upper:] [:lower:]` </s> (${f})" \
 >> $etcdir/ellavator_train.transcription;done;
 
 cd ..
 
 echo "data preparation done"
-
-
-
-
-
--- ellavator/run_adaptation.sh
+++ ellavator/run_adaptation.sh
@@ -1,21 +1,66 @@
 #!/bin/bash
 
+# the executables required for adapting the acoustic model
 bw='/usr/local/libexec/sphinxtrain/bw'
 mllr_solve='/usr/local/libexec/sphinxtrain/mllr_solve'
 map_adapt='/usr/local/libexec/sphinxtrain/map_adapt'
-cur_dir=`pwd`
 
-if [ "$#" -ne 1 ]; then
+# This (ele_proj) is the directory of the Ellavator project on the github.
+# Change the ele_proj path below to your own project.
+# The (etcdir) is the direcotry inside the am_training/ellavator repository
+# Change this to your own path.
+
+cur_dir=`pwd`
+ele_proj='/Users/arif/workspace/Ellavator_Dir/Ellavator'
+ac_dir=$ele_proj/'src/main/resources/en-us-adapt/'
+etcdir=$cur_dir/'etc'
+
+if [ "$#" -ne 2 ]; then
   echo "==================================="
-  echo "Usage: $0 mllr OR $0 map" >&2
+  echo "Usage: $0 mllr|map close|noise|both " >&2
+  echo " "
+  echo "Please choose either mllr or map as adaptation method" >&2
+  echo "and close, noise or both as data options to use in adaptation" >&2
   echo "==================================="
   exit 1
 fi
 
-adapt=$1
-fileids='etc/ellavator_train.fileids'
-dic='etc/ellavator.dic'
-trans='etc/ellavator_train.transcription'
+adapt_method=$1
+adapt_data=$2
+
+if [ $adapt_data = both ]; then
+adapt_data='noise|close'
+fi
+
+ln -s $ele_proj/prompts/build/resources/data/ wav
+
+cd wav 
+
+txtids=`ls *.txt | grep 'en' | grep -E $adapt_data | cut -d . -f 1`
+
+echo "==================================="
+echo "adaptation on $adapt_data data"
+echo "==================================="
+fileids=`ls *.wav | grep 'en' | grep -E $adapt_data | cut -d . -f 1`
+
+echo "making fileids file"
+rm -rf $etcdir/ellavator_train.fileids
+for f in $fileids;do `echo $f >> $etcdir/ellavator_train.fileids`;done;
+
+echo "making training transcription file"
+rm -rf $etcdir/ellavator_train.transcription
+for f in $txtids;do echo "<s> `cat $f.txt | tr [:upper:] [:lower:]` </s> (${f})" \
+>> $etcdir/ellavator_train.transcription;done;
+
+cd $cur_dir
+
+echo "==================================="
+echo "data preparation done"
+echo "==================================="
+
+fileids=$etcdir/'ellavator_train.fileids'
+dic=$etcdir/'ellavator.dic'
+trans=$etcdir/'ellavator_train.transcription'
 
 if [ ! -f cmusphinx-en-us-5.2.tar.gz ]; then
 rm -rf $cur_dir/en-us && echo "Deleting old model"
@@ -45,7 +90,7 @@ $bw -hmmdir en-us -moddeffn en-us/mdef.txt -ts2cbfn .cont. -feat 1s_c_d_dd \
 		-cmn current -agc none -lda en-us/feature_transform -cepdir etc -dictfn $dic \
 		-ctlfn $fileids -lsnfn $trans -accumdir $cur_dir
 
-if [ $adapt = mllr ]; then
+if [ $adapt_method = mllr ]; then
 echo "==================================="
 echo "MLLR adaptation"
 echo "==================================="
@@ -53,7 +98,7 @@ $mllr_solve -meanfn en-us/means -varfn en-us/variances -outmllrfn mllr_matrix -a
 
 fi
 
-if [ $adapt = map ]; then
+if [ $adapt_method = map ]; then
 
 echo "==================================="
 echo "MAP adaptation"
@@ -63,3 +108,9 @@ $map_adapt -moddeffn en-us/mdef.txt -ts2cbfn .cont. -meanfn en-us/means -varfn e
 		-mixwfn en-us/mixture_weights -tmatfn en-us/transition_matrices -accumdir $cur_dir -mapmeanfn en-us-adapt/means \
         -mapvarfn en-us-adapt/variances -mapmixwfn en-us-adapt/mixture_weights -maptmatfn en-us-adapt/transition_matrices
 fi
+
+cp en-us/feat.params en-us/mdef en-us-adapt/ || exit 1;
+
+cp -r en-us-adapt/. $ac_dir
+
+echo "adapted model ready for use"
